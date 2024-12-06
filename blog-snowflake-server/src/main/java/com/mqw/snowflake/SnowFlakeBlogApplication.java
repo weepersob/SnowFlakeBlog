c1a98d525a1276c22b433897117ed544393919e0
weepersob <2458832862@qq.com>
2024-12-06 12:37:42 +0800

    SnowFlakeBlog项目初始化

---
+++ blog-snowflake-server/src/main/java/com/mqw/snowflake/SnowFlakeBlogApplication.java
@@ -0,0 +1,12 @@
+package com.mqw.snowflake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowFlakeBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SnowFlakeBlogApplication.class, args);
+    }
+
+}
